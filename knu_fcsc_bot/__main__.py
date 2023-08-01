26edb21e7f106cddc227d3eec223e17a87fc83c7
antos07 <trotsenko.ant07@gmail.com>
2023-08-01 12:48:07 +0200

    Add command line argument parser

--- knu_fcsc_bot/__main__.py
+++ knu_fcsc_bot/__main__.py
@@ -1,4 +1,6 @@
 import os
+from argparse import ArgumentParser
+from dataclasses import dataclass
 
 from loguru import logger
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
@@ -12,7 +14,33 @@ from knu_fcsc_bot.logginig import (redirect_standard_logging_to_loguru,
 from knu_fcsc_bot.usecases import list_allowed_chat_ids_usecase
 
 
+@dataclass
+class StartupOptions:
+    """A collection of startup options"""
+    verbose_logging: bool = False
+    use_webhook: bool = False
+
+
+def get_startup_options() -> StartupOptions:
+    """Parses command line args into StartupOptions"""
+    parser = ArgumentParser()
+    parser.add_argument('--verbose', '-v', action='store_true',
+                        help='If present, enables DEBUG log-level', )
+    parser.add_argument('--use-webhook', '-w',
+                        action='store_true',
+                        help='If present, switches to using webhook instead '
+                             'of long polling')
+
+    args = parser.parse_args()
+    return StartupOptions(
+        verbose_logging=args.verbose,
+        use_webhook=args.use_webhook,
+    )
+
+
 async def app_post_init(app: Application) -> None:
+    """Called after Application was initialized to perform additional set up.
+    Currently, only loads allowed chats from the database"""
     # Set allowed chats
     session = app.bot_data['AsyncSession']()
     async with session:
@@ -22,9 +50,12 @@ async def app_post_init(app: Application) -> None:
 
 
 def main():
+    startup_options = get_startup_options()
+
     redirect_standard_logging_to_loguru()
     disable_low_level_logs()
-    set_logging_level('INFO')
+    log_lvl = 'INFO' if not startup_options.verbose_logging else 'DEBUG'
+    set_logging_level(log_lvl)
 
     app = (ApplicationBuilder()
            .token(os.environ['BOT_TOKEN'])
@@ -45,6 +76,9 @@ def main():
         UpdateType.CALLBACK_QUERY,
         UpdateType.MESSAGE,
     ]
+
+    if startup_options.use_webhook:
+        raise NotImplementedError('Webhooks are not implemented yet')
     app.run_polling(allowed_updates=allowed_updates)
 
 
